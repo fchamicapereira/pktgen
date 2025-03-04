c5ba23890f0b40e86ca86eb745892b546e428d54
Francisco Pereira <fchamicapereira@gmail.com>
2025-03-04 13:56:08 +0000

    Building write packets for kvs

--- src/pktgen.cpp
+++ src/pktgen.cpp
@@ -212,7 +212,7 @@ static void generate_template_packet(byte_t *pkt, uint16_t size) {
     current_pkt_size += sizeof(kvs_hdr_t);
     current_pkt_ptr += sizeof(kvs_hdr_t);
 
-    kvs_hdr->op = KVS_OP_GET;
+    kvs_hdr->op = KVS_OP_PUT;
     memset(kvs_hdr->key, 0, KEY_SIZE_BYTES);
     memset(kvs_hdr->value, 0, MAX_VALUE_SIZE_BYTES);
     kvs_hdr->status      = KVS_STATUS_CACHE_MISS;
